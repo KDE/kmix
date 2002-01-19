fcf9049630505ff5f2e8ed4e62b69f0fa84a0fe2
Ellis Whitehead <kde@ellisw.net>
2002-01-19 06:20:04 +0000

    KAccel::insertAction() => insert()
    svn path=/trunk/kdemultimedia/kmix/; revision=132478

--- mixdevicewidget.cpp
+++ mixdevicewidget.cpp
@@ -97,11 +97,11 @@ MixDeviceWidget::MixDeviceWidget(Mixer *mixer, MixDevice* md,
    m_updateTimer->start( 200, FALSE );
 
    m_keys=new KGlobalAccel(this,"Keys");
-   m_keys->insertAction( "Increase volume", i18n( "Increase volume" ), QString::null,
+   m_keys->insert( "Increase volume", i18n( "Increase volume" ), QString::null,
       KShortcut(), KShortcut(), this, SLOT( increaseVolume() ) );
-   m_keys->insertAction( "Decrease volume", i18n( "Decrease volume" ), QString::null,
+   m_keys->insert( "Decrease volume", i18n( "Decrease volume" ), QString::null,
       KShortcut(), KShortcut(), this, SLOT( decreaseVolume() ) );
-   m_keys->insertAction( "Toggle mute", i18n( "Toggle mute" ), QString::null,
+   m_keys->insert( "Toggle mute", i18n( "Toggle mute" ), QString::null,
       KShortcut(), KShortcut(), this, SLOT( toggleMuted() ) );
    m_keys->updateConnections();
 };
