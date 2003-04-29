e33eb3bc4ebe1dc3778112f96d90ccea5560c0b3
Christian Esken <esken@kde.org>
2003-04-29 21:33:55 +0000

    VerticalText widget.
    svn path=/trunk/kdemultimedia/kmix/; revision=222842

---
+++ VerticalText.cpp
@@ -0,0 +1,42 @@
+/*
+ * KMix -- KDE's full featured mini mixer
+ *
+ *
+ * Copyright (C) 2003 Christian Esken <esken@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "VerticalText.h"
+#include <qpainter.h>
+#include <kdebug.h>
+
+
+VerticalText::VerticalText(QWidget * parent, const char * name, WFlags f) : QWidget(parent,name,f)
+{
+	resize(20,100 /*parent->height() */ );
+	setFixedSize(20,100);
+}
+
+VerticalText::~VerticalText() {
+}
+
+
+void VerticalText::paintEvent ( QPaintEvent * /*event*/ ) {
+    //kdDebug() << "paintEvent()\n";
+	QPainter paint(this);
+	paint.rotate(270);
+	paint.drawText(-height()+2,width(),name());
+}
