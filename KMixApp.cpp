bf6cf9e7043d9fdc37bc48791f7658ebc4a2cc4f
Christian Esken <esken@kde.org>
2003-06-27 22:20:37 +0000

    The big cleanup round (putting every class in own file): Add KMixApp.(cpp|h)
    svn path=/trunk/kdemultimedia/kmix/; revision=234226

---
+++ KMixApp.cpp
@@ -0,0 +1,67 @@
+/*
+ * KMix -- KDE's full featured mini mixer
+ *
+ * $Id$
+ *
+ * Copyright (C) 2000 Stefan Schimanski <schimmi@kde.org>
+ * Copyright (C) 2001 Preston Brown <pbrown@kde.org>
+ * Copyright (C) 2003 Sven Leiber <s.leiber@web.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "KMixApp.h"
+#include "kmix.h"
+
+KMixApp::KMixApp()
+    : KUniqueApplication(), m_kmix( 0 )
+{
+}
+
+
+KMixApp::~KMixApp()
+{
+	delete m_kmix;
+}
+
+
+int
+KMixApp::newInstance()
+{
+	if ( m_kmix )
+	{
+		m_kmix->show();
+	}
+	else
+	{
+		m_kmix = new KMixWindow;
+		connect(this, SIGNAL(stopUpdatesOnVisibility()), m_kmix, SLOT(stopVisibilityUpdates()));
+		if ( isRestored() && KMainWindow::canBeRestored(0) )
+		{
+			m_kmix->restore(0, FALSE);
+		}
+	}
+
+	return 0;
+}
+
+
+void KMixApp::quitExtended() {
+    // This method is here for quiting from the dock icon: When directly calling
+    // quit(), the main window will be hidden before saving the configuration.
+    // isVisible() would return on quit always false (which would be bad).
+    emit stopUpdatesOnVisibility();
+    quit();
+}
---
+++ KMixApp.h
@@ -0,0 +1,26 @@
+#ifndef KMixApp_h
+#define KMixApp_h
+
+#include <kuniqueapplication.h>
+
+class KMixWindow;
+
+class KMixApp : public KUniqueApplication
+{
+Q_OBJECT
+ public:
+    KMixApp();
+    ~KMixApp();
+    int newInstance ();
+
+    public slots:
+    void quitExtended();  // For a hack on visibility()
+
+ signals:
+    void stopUpdatesOnVisibility();
+    
+ private:
+    KMixWindow *m_kmix;
+};
+
+#endif
