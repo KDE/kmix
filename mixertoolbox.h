212cc74f4584e4eef97d42b96a6806fa82354406
Christian Esken <esken@kde.org>
2004-08-24 16:06:16 +0000

    MixerToolBox (containing initMixer()
    svn path=/trunk/kdemultimedia/kmix/; revision=340511

---
+++ mixertoolbox.h
@@ -0,0 +1,20 @@
+#ifndef MIXERTOOLBOX_H
+#define MIXERTOOLBOX_H
+
+#include "qptrlist.h"
+
+class Mixer;
+
+/**
+ * This toolbox contains various static methods that are shared throughout KMix.
+ * It only contains no-GUI code. The shared with-GUI code is in KMixToolBox
+ * The reason, why it is not put in a common base class is, that the classes are
+ * very different and cannot be changed (e.g. KPanelApplet) without major headache.
+ */
+class MixerToolBox {
+ public:
+    static void initMixer(QPtrList<Mixer>&, bool);
+};
+    
+
+#endif
