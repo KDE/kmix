70b2415af680133f2d296fa2a4592b4fb50232fc
James Ots <kde@jamesots.com>
2005-08-01 08:00:20 +0000

    Centre the kmix slider on the icon correctly, and don't have the slider overlapping the icon.
    svn path=/branches/KDE/3.5/kdemultimedia/kmix/; revision=441950

--- kmixdockwidget.cpp
+++ kmixdockwidget.cpp
@@ -253,8 +253,8 @@ KMixDockWidget::mousePressEvent(QMouseEvent *me)
 		}
 
 		int h = _dockAreaPopup->height();
-		int x = this->mapToGlobal( QPoint( 0, 0 ) ).x() - this->width()/2;
-		int y = this->mapToGlobal( QPoint( 0, 0 ) ).y() - h + this->height();
+		int x = this->mapToGlobal( QPoint( 0, 0 ) ).x() + this->width()/2 - _dockAreaPopup->width()/2;
+		int y = this->mapToGlobal( QPoint( 0, 0 ) ).y() - h;
 		if ( y - h < 0 )
 			y = y + h - this->height();
 
