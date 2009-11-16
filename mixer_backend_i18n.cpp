6d768661a10342f36f902f7547778727ba7dc385
Andrew Coles <andrew.i.coles@googlemail.com>
2009-11-16 14:16:20 +0000

    Proof-reading.  It looks like the first message was pasted into the middle of the second, so I've attempted to recreate it.
    svn path=/trunk/KDE/kdemultimedia/kmix/; revision=1050051

--- mixer_backend_i18n.cpp
+++ mixer_backend_i18n.cpp
@@ -22,8 +22,8 @@
 QString Mixer_Backend::translateKernelToWhatsthis(const QString &kernelName)
 {
         if (kernelName == "Mic:0") return i18n("Recording level of the microphone input.");
-	else if (kernelName == "Master:0") return i18n("Controls the volume of the front speakers or all speakers (depending on your soundcard model). If you use a digital output, you might need to also use other controls like ADC or DAC. For headphones soundcards often supply a Headphone control.");
-	else if (kernelName == "PCM:0") return i18n("Most media like MP3 or Videos are played back using PCM. For playControls the volume of the front speakers or all speakers (depending on your soundcard model). If you use a digital output, you might need to also use other controls like ADC or DAC.");
+	else if (kernelName == "Master:0") return i18n("Controls the volume of the front speakers or all speakers (depending on your soundcard model). If you use a digital output, you might need to also use other controls like ADC or DAC. For headphones, soundcards often supply a Headphone control.");
+	else if (kernelName == "PCM:0") return i18n("Most media, such as MP3s or Videos, are played back using the PCM channel. As such, the playback volume of such media is controlled by both this and the Master or Headphone channels.");
 	else if (kernelName == "Headphone:0") return i18n("Controls the headphone volume. Some soundcards include a switch that must be manually activated to enable the headphone output.");
 	else return i18n("---"); 
 }
