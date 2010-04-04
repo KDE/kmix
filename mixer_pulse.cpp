acc5eb6cf36e95eaacabdca13ac469b6041759d9
Colin Guthrie <cguthrie@mandriva.org>
2010-04-04 11:38:25 +0000

    kmix(pulse): Ensure kmix does not save/restore our volumes. We handle that lower down the audio stack.
    svn path=/trunk/KDE/kdemultimedia/kmix/; revision=1110890

--- mixer_pulse.cpp
+++ mixer_pulse.cpp
@@ -698,7 +698,7 @@ void Mixer_PULSE::addDevice(devinfo& dev, bool capture)
     if (dev.chanMask != Volume::MNONE) {
         Volume v(dev.chanMask, PA_VOLUME_NORM, PA_VOLUME_MUTED, !capture/*mute switch*/, capture);
         setVolumeFromPulse(v, dev);
-        MixDevice* md = new MixDevice( _mixer, dev.name, dev.description, dev.icon_name);
+        MixDevice* md = new MixDevice( _mixer, dev.name, dev.description, dev.icon_name, true);
         if (capture)
             md->addCaptureVolume(v);
         else
