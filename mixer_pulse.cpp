558c0fca765276245ce266e60d3b8eb182d5cc53
Colin Guthrie <cguthrie@mandriva.org>
2010-04-04 11:37:25 +0000

    kmix(pulse): Basic structure for getting information from pulse and registering for the relevant callbacks.
    This adds a structure whereby the first pulse mixer object created
    will connect to PA and register for the necessary callbacks. The
    subsequent objects will not connect a second time.
    
    What now needs to be done is to store the information about
    each sink/source in some kind of QMap or similar such that
    the information can be presented.
    
    We currently employ an event loop that runs while we try and connect.
    This loop is exited on failure or when we've received the callbacks
    about sink/source that allow us to fill in our cache.
    
    svn path=/trunk/KDE/kdemultimedia/kmix/; revision=1110850

--- mixer_pulse.cpp
+++ mixer_pulse.cpp
@@ -20,6 +20,7 @@
  */
 
 #include <cstdlib>
+#include <QEventLoop>
 
 #include "mixer_pulse.h"
 #include "mixer.h"
@@ -28,8 +29,160 @@
 #include <pulse/glib-mainloop.h>
 #include <pulse/ext-stream-restore.h>
 
+static unsigned int refcount = 0;
 static pa_context *context = NULL;
 static pa_glib_mainloop *mainloop = NULL;
+static QEventLoop *s_connectionEventloop = NULL;
+static bool s_pulseActive = true;
+static int s_OutstandingRequests = 0;
+
+static void dec_outstanding() {
+    if (s_OutstandingRequests <= 0)
+        return;
+
+    if (--s_OutstandingRequests <= 0)
+    {
+        if (s_connectionEventloop) {
+            s_connectionEventloop->exit(0);
+            s_connectionEventloop = NULL;
+        }
+    }
+}
+
+void sink_cb(pa_context *c, const pa_sink_info *i, int eol, void *) {
+
+    Q_ASSERT(c == context);
+
+    if (eol < 0) {
+        if (pa_context_errno(c) == PA_ERR_NOENTITY)
+            return;
+
+        kWarning(67100) << "Sink callback failure";
+        return;
+    }
+
+    if (eol > 0) {
+        dec_outstanding();
+        return;
+    }
+
+    // Do something....
+    kDebug(67100) << "Got some info about sink: " << i->name;
+}
+
+void source_cb(pa_context *c, const pa_source_info *i, int eol, void *) {
+
+    Q_ASSERT(c == context);
+
+    if (eol < 0) {
+        if (pa_context_errno(c) == PA_ERR_NOENTITY)
+            return;
+
+        kWarning(67100) << "Source callback failure";
+        return;
+    }
+
+    if (eol > 0) {
+        dec_outstanding();
+        return;
+    }
+
+    // Do something....
+    kDebug(67100) << "Got some info about source: " << i->name;
+}
+
+void subscribe_cb(pa_context *c, pa_subscription_event_type_t t, uint32_t index, void *) {
+
+    Q_ASSERT(c == context);
+
+    switch (t & PA_SUBSCRIPTION_EVENT_FACILITY_MASK) {
+        case PA_SUBSCRIPTION_EVENT_SINK:
+            if ((t & PA_SUBSCRIPTION_EVENT_TYPE_MASK) == PA_SUBSCRIPTION_EVENT_REMOVE) {
+                // Todo: Remove our cache of this sink (of 'index')
+            } else {
+                pa_operation *o;
+                if (!(o = pa_context_get_sink_info_by_index(c, index, sink_cb, NULL))) {
+                    kWarning(67100) << "pa_context_get_sink_info_by_index() failed";
+                    return;
+                }
+                pa_operation_unref(o);
+            }
+            break;
+
+        case PA_SUBSCRIPTION_EVENT_SOURCE:
+            if ((t & PA_SUBSCRIPTION_EVENT_TYPE_MASK) == PA_SUBSCRIPTION_EVENT_REMOVE) {
+                // Todo: Remove our cache of this source (of 'index')
+            } else {
+                pa_operation *o;
+                if (!(o = pa_context_get_source_info_by_index(c, index, source_cb, NULL))) {
+                    kWarning(67100) << "pa_context_get_source_info_by_index() failed";
+                    return;
+                }
+                pa_operation_unref(o);
+            }
+            break;
+
+    }
+}
+
+
+static void context_state_callback(pa_context *c, void *)
+{
+    Q_ASSERT(c == context);
+
+    switch (pa_context_get_state(c)) {
+        case PA_CONTEXT_UNCONNECTED:
+        case PA_CONTEXT_CONNECTING:
+        case PA_CONTEXT_AUTHORIZING:
+        case PA_CONTEXT_SETTING_NAME:
+            break;
+
+        case PA_CONTEXT_READY:
+            // Attempt to load things up
+            pa_operation *o;
+
+            pa_context_set_subscribe_callback(c, subscribe_cb, NULL);
+
+            if (!(o = pa_context_subscribe(c, (pa_subscription_mask_t)
+                                           (PA_SUBSCRIPTION_MASK_SINK|
+                                            PA_SUBSCRIPTION_MASK_SOURCE), NULL, NULL))) {
+                kWarning(67100) << "pa_context_subscribe() failed";
+                return;
+            }
+            pa_operation_unref(o);
+
+            if (!(o = pa_context_get_sink_info_list(c, sink_cb, NULL))) {
+                kWarning(67100) << "pa_context_get_sink_info_list() failed";
+                return;
+            }
+            pa_operation_unref(o);
+            s_OutstandingRequests++;
+
+            if (!(o = pa_context_get_source_info_list(c, source_cb, NULL))) {
+                kWarning(67100) << "pa_context_get_source_info_list() failed";
+                return;
+            }
+            pa_operation_unref(o);
+            s_OutstandingRequests++;
+
+            break;
+
+        case PA_CONTEXT_FAILED:
+            s_pulseActive = false;
+            if (s_connectionEventloop) {
+                s_connectionEventloop->exit(0);
+                s_connectionEventloop = NULL;
+            }
+            break;
+
+        case PA_CONTEXT_TERMINATED:
+        default:
+            s_pulseActive = false;
+            /// @todo Deal with reconnection...
+            break;
+    }
+}
+
 
 Mixer_Backend* PULSE_getMixer( Mixer *mixer, int devnum )
 {
@@ -43,23 +196,43 @@ Mixer_PULSE::Mixer_PULSE(Mixer *mixer, int devnum) : Mixer_Backend(mixer, devnum
 {
    if ( devnum == -1 )
       m_devnum = 0;
+
+   if (0 == refcount++)
+   {
+       mainloop = pa_glib_mainloop_new(g_main_context_default());
+       g_assert(mainloop);
+
+       pa_mainloop_api *api = pa_glib_mainloop_get_api(mainloop);
+       g_assert(api);
+
+       context = pa_context_new(api, "KMix KDE 4");
+       g_assert(context);
+
+       // We create a simple event loop to allow the glib loop
+       // to iterate until we've connected or not to the server.
+       s_connectionEventloop = new QEventLoop;
+
+       // (cg) Convert to PA_CONTEXT_NOFLAGS when PulseAudio 0.9.19 is required
+       if (pa_context_connect(context, NULL, static_cast<pa_context_flags_t>(0), 0) >= 0) {
+           pa_context_set_state_callback(context, &context_state_callback, s_connectionEventloop);
+           // Now we block until we connect or otherwise...
+           s_connectionEventloop->exec();
+       }
+   }
 }
 
 Mixer_PULSE::~Mixer_PULSE()
 {
-   close();
+    if (0 == --refcount)
+    {
+        pa_context_unref(context);
+        pa_glib_mainloop_free(mainloop);
+    }
 }
 
 int Mixer_PULSE::open()
 {
     kDebug(67100) <<  "Trying Pulse sink";
-    mainloop = pa_glib_mainloop_new(g_main_context_default());
-    g_assert(mainloop);
-    pa_mainloop_api *api = pa_glib_mainloop_get_api(mainloop);
-    g_assert(api);
-
-    context = pa_context_new(api, "KMix KDE 4");
-    g_assert(context);
       //return Mixer::ERR_OPEN;
  
 /* 
@@ -89,16 +262,6 @@ int Mixer_PULSE::open()
 
 int Mixer_PULSE::close()
 {
-    if (context)
-    {
-        pa_context_unref(context);
-        context = NULL;
-    }
-    if (mainloop)
-    {
-        pa_glib_mainloop_free(mainloop);
-        mainloop = NULL;
-    }
     return 1;
 }
 
