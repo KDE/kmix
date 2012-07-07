7d6c4acabcc472963cca9f738bebb48634f5512d
Trever Fischer <tdfischer@fedoraproject.org>
2012-07-07 12:43:35 +0300

    Start writing kmixd

---
+++ src/daemon/KMixDApp.h
@@ -0,0 +1,39 @@
+//-*-C++-*-
+/*
+ * KMix -- KDE's full featured mini mixer
+ *
+ * Copyright (C) Trever Fischer <tdfischer@fedoraproject.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef KMixApp_h
+#define KMixApp_h
+
+#include <QtCore/QCoreApplication>
+
+class KMixDeviceManager;
+
+class KMixDApp : public QCoreApplication
+{
+    Q_OBJECT
+public:
+    KMixDApp(int &argc, char **argv);
+    ~KMixDApp();
+    int start();
+private:
+    KMixDeviceManager *m_devManager;
+};
+
+#endif
