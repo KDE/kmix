7fb96c1bc5275afb7985d2754adf9cc75e6c1156
Trever Fischer <tdfischer@fedoraproject.org>
2012-09-24 23:55:14 +0200

    Add an icon, and code cleanup

--- src/gui2/ControlSlider.cpp
+++ src/gui2/ControlSlider.cpp
@@ -3,6 +3,7 @@
 #include <QtGui/QLabel>
 #include <QtGui/QSlider>
 #include <QtCore/QSignalMapper>
+#include <KDE/KIcon>
 
 #include "control_interface.h"
 
@@ -10,14 +11,24 @@ ControlSlider::ControlSlider(org::kde::KMix::Control *control, QWidget *parent)
     : QWidget(parent)
     , m_control(control)
 {
-    QVBoxLayout *layout = new QVBoxLayout();
-    QLabel *label = new QLabel(m_control->displayName(), this);
+    QVBoxLayout *layout = new QVBoxLayout(this);
     setLayout(layout);
 
+    QWidget *labelContainer = new QWidget(this);
+    QHBoxLayout *labelLayout = new QHBoxLayout(labelContainer);
+    labelContainer->setLayout(labelLayout);
+
+    QLabel *label = new QLabel(m_control->displayName(), labelContainer);
+    QLabel *iconLabel = new QLabel(labelContainer);
+    KIcon icon(control->iconName());
+    iconLabel->setPixmap(icon.pixmap(QSize(32, 32)));
+
+    labelLayout->addWidget(iconLabel);
+    labelLayout->addWidget(label);
+
     QWidget *sliderContainer = new QWidget(this);
-    layout->addWidget(label);
-    layout->addWidget(sliderContainer);
     QHBoxLayout *sliderLayout = new QHBoxLayout(sliderContainer);
+
     QSignalMapper *mapper = new QSignalMapper(this);
     connect(mapper, SIGNAL(mapped(int)), this, SLOT(updateVolume(int)));
     for(int i = 0;i<control->channels();i++) {
@@ -30,6 +41,9 @@ ControlSlider::ControlSlider(org::kde::KMix::Control *control, QWidget *parent)
         connect(slider, SIGNAL(valueChanged(int)), mapper, SLOT(map()));
     }
     connect(control, SIGNAL(volumeChanged(int)), this, SLOT(volumeChange(int)));
+
+    layout->addWidget(labelContainer);
+    layout->addWidget(sliderContainer);
 }
 
 ControlSlider::~ControlSlider()
